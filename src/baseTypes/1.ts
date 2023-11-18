865cb7a4709ee35a6f96fb08e519e10a0081298b
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-11-18 23:14:32 +0200

    feature(basics-generics): added tasks for basic types and generics

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -2,21 +2,26 @@
 
 let age: number = 50;
 let name: string = 'Max';
-let toggle:boolean = true;
+let toggle: boolean = true;
 let empty: null = null;
 let notInitialize: unknown;
-let callback = (a: number): number => { return 100 + a };
+let callback = (a: number): number => {
+    return 100 + a
+};
 
 export {};
 
-type Dog = {
-
+interface Dog {
+    bark(): void;
 }
 
-type Cat = {
-
+interface Cat {
+    meow(): void;
 }
 
-function isDog(animal: Dog | Cat):  animal is Dog{
-    return 'bark' in animal;
+function isDog(animal: Dog | Cat): asserts animal is Dog {
+    if (!('bark' in animal)) {
+        throw new Error('Not a dog');
+    }
+    // return 'bark' in animal;
 }
